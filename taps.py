4b56b3c5fe9767d25cf3ecbb9bbd7aeeba445382
Adam Anderson <horatio.blackwood@gmail.com>
2016-04-03 21:31:50 -0700

    Refactored into taps.py module.

--- taps.py
+++ taps.py
@@ -1,13 +1,13 @@
 # Author:       Adam Anderson
 # Date:         Mar 15, 2016
-# Module:       parser.py
+# Module:       taps.py
 # Python:       3.4.2
 
-import tools
 import string
 from enum import Enum
 
 
+# --- DATA OBJECTS
 class WordType(Enum):
     """
     An enumeration of WordTypes from the Parts of Speech.
@@ -16,6 +16,16 @@ class WordType(Enum):
     ADJECTIVE = 1
     NOUN = 2
     PREPOSITION = 3
+    COMMAND = 5
+
+
+class SentenceType(Enum):
+    """
+    An enumeration of sentence types.
+    """
+    COMMAND = 1
+    VERB_NOUN = 2
+    VERB_NOUN_PREP_NOUN = 3
 
 
 class ParserContext(object):
@@ -25,18 +35,37 @@ class ParserContext(object):
 
     The ParserContext is not planned to be persisted between application runs, however ,if this state proves to be
     valuable enough to keep between runs of the game, it could be persisted.
-
-    :param adjectives - the list of adjectives the parser should know about..
-    :param nouns - the list of nouns the parser should know about.
-    :param prepositions - the list of prepositions the parser should know about.
-    :param verbs - the list of verbs the parser should know about.
     """
-    def __init__(self,adjectives=[], nouns=[], prepositions=[], verbs=[]):
+    def __init__(self):
         self.last_noun = None
+
+
+class GameDictionary(object):
+    """
+    An object that encapsulates the words known by the game.
+
+    This class sort of duplicates functionality in the Parser Context - wondering if this replaces the dictionary
+    functions of that class?
+    """
+
+    def __init__(self, adjectives=[], commands=[], nouns=[], prepositions=[], verbs=[]):
+        """
+        Initializes the game dictionary with the supplied Lists of words.
+        :param verbs - the list of verbs (Strings) that the game knows.
+        :param nouns - the list of nouns (Strings) that the game knows.
+        :param adjectives - the list of adjectives (Strings) that the game knows.
+        :param prepositions - the list of prepositions (Strings) that the game knows.
+        :param commands - the list of commands (Strings) that the game knows.  Commands are special words that are
+        detected by the parser because they are special single-word commands that tell the game processor to do
+        something unreleated to advancing the story of the game.  For example:  'inventory' might display the player's
+        inventory, or 'quit' might exit the game.  Neither of them have anything to do with the player character or what
+        is going on in the story right now.
+        """
         self.verbs = verbs
         self.nouns = nouns
-        self.prepositions = prepositions
         self.adjectives = adjectives
+        self.prepositions = prepositions
+        self.commands = commands
 
 
 class StatementStructure(object):
@@ -55,33 +84,53 @@ class StatementStructure(object):
         return tuple(s)
 
 
-class InputParser(object):
+class Result(object):
+    """
+    The object returned by the InputParser.  The game implementation uses this object to determine what happened
+    as a result of the user's input.
+    """
 
-    def __init__(self):
-        # Basically only two structures are supported VERB NOUN and VERB NOUN PREPOSITION NOUN, but each noun
-        # can optionally have an adjective.
-        self.valid_structures = [(WordType.VERB, WordType.NOUN),
+    def __init__(self, type, verb, direct_obj, preposition=None, indirect_obj=None):
+        """ Initializes the Result.
+
+            :param type - the sentence type.
+            :param verb - the verb in the sentence.  A verb is required for all supported sentences.  As such, None
+            is not valid.
+            :param direct_obj - the direct object (the object the verb is modifying or acting upon).  A direct_object
+            is present in all supported sentences, so None is not valid for this object.
+            :param preposition - a preposition, if one is present.
+            :param indirect_obj - a second noun in the sentence, a direct object or object of a preposition.
+        """
+        self.sentence_type = type
+        self.verb = verb
+        self.direct_obj = direct_obj
+        self.preposition = preposition
+        self.indirect_obj = indirect_obj
+
+
+# --- PROCESSORS
+class InputParser(object):
+    """
+    Class wrapping the function and state of the input parser.
+    """
+
+    def __init__(self, game_dict):
+        # Store the game dictionary.
+        self.game_dict = game_dict
+
+        # Initialize Parser Context
+        self.parser_context = ParserContext()
+
+        # Basically only two structures are currently supported VERB NOUN and VERB NOUN PREPOSITION NOUN, but
+        # each noun can optionally have an adjective.
+        self.valid_structures = [(WordType.COMMAND,),
+                                 (WordType.VERB, WordType.NOUN),
                                  (WordType.VERB, WordType.ADJECTIVE, WordType.NOUN),
                                  (WordType.VERB, WordType.NOUN, WordType.PREPOSITION, WordType.NOUN),
                                  (WordType.VERB, WordType.NOUN, WordType.PREPOSITION, WordType.ADJECTIVE, WordType.NOUN),
                                  (WordType.VERB, WordType.ADJECTIVE, WordType.NOUN, WordType.PREPOSITION, WordType.NOUN),
                                  (WordType.VERB, WordType.ADJECTIVE, WordType.NOUN, WordType.PREPOSITION, WordType.ADJECTIVE, WordType.NOUN)]
 
-        # Initialize Parser Context
-        a, n, p, v = self.__load_data()
-        self.parser_context = ParserContext(a, n, p, v)
-
-    def __load_data(self):
-        """
-        Read in the known words from files and return lists of words.
-        :return: A tuple of lists of words.
-        """
-        adjectives = tools.read_word_file("data/adjectives.txt")
-        nouns = tools.read_word_file("data/nouns.txt")
-        prepositions = tools.read_word_file("data/prepositions.txt")
-        verbs = tools.read_word_file("data/verbs.txt")
-
-        return adjectives, nouns, prepositions, verbs
 
     def sanitize_input(self, input_string):
         """
@@ -113,18 +162,31 @@ class InputParser(object):
         :return:
         """
         words = self.sanitize_input(input_string)
-        unrecognized = []
         structure = []
+        unrecognized = []
+
+        # If the wordlist is a command, it will be only one word long - this is the only valid one-word structure.
+        # If its not a recognized command, return an Unrecognized Structure.
+        if len(words) is 1:
+            if words[0] in self.game_dict.commands:
+                structure.append((WordType.COMMAND, words[0]))
+            else:
+                unrecognized.append(words[0])
+
+            return StatementStructure(tuple(structure), unrecognized)
+
+
+        # If the wordlist is a multi-word sentence, process it to the fullest extent.
         nouns_found = 0
 
         for word in words:
             word = word.lower()
             # VERB
-            if word in self.parser_context.verbs:
+            if word in self.game_dict.verbs:
                 structure.append((WordType.VERB, word))
 
             # NOUN
-            elif word in self.parser_context.nouns:
+            elif word in self.game_dict.nouns:
                 structure.append((WordType.NOUN, word))
 
                 # Update Parser context with most recently referenced noun.
@@ -132,11 +194,11 @@ class InputParser(object):
                 self.parser_context.last_noun = word
 
             # PREP
-            elif word in self.parser_context.prepositions:
+            elif word in self.game_dict.prepositions:
                 structure.append((WordType.PREPOSITION, word))
 
             # ADJ
-            elif word in self.parser_context.adjectives:
+            elif word in self.game_dict.adjectives:
                 structure.append((WordType.ADJECTIVE, word))
 
             # If we didnt recognize the word as a Verb, Noun or Preposition, add it to the unrecognized word list.
@@ -151,14 +213,10 @@ class InputParser(object):
 
         return StatementStructure(tuple(structure), unrecognized)
 
-
     def parse_input(self, input_string):
         """
         This method takes an input string, sanitizes it, performs checks to ensure it is valid, and generates a
         StatementStructure object.
-
-         Using the supplied game_state and adenture objects, the parser is able to update the game state with any relevant
-         changes that have occured as a result of the supplied input..
 
         :param input_string: the string to parse.
         :return: None
@@ -174,3 +232,5 @@ class InputParser(object):
             print("Structure supported:", statement.get_structure())
         else:
             print("Structure not supported:", statement.get_structure())
+
+        return statement
--- test_main.py
+++ test_main.py
@@ -1,7 +1,61 @@
-import parser
+import taps
 
+
+def read_word_file(file):
+    """
+    Reads a file full of words (one word per line) and puts them into a list.  Any lines that begin with a '#'
+    character are ignored.
+
+    :rtype: List of words read in from the file.
+    """
+    words = []
+    with open(file, 'r') as wordfile:
+        w = wordfile.readlines()
+
+    for word in w:
+        word = word.strip()
+        if not word.startswith("#") and len(word) > 0:
+            words.append(word.lower())
+
+    return words
+
+
+
+def sort_file(infile, outfile=None):
+    """
+    Reads in a file line by line, and sorts the lines alphabetically.
+
+    :param infile -  the name of the file that has lines to be sorted.
+    :param outfile - the name of the file to write the data to, if no file is provided writes to the same
+                     file name as inflie.
+
+    :return: None -  but does write out the sorted file
+    """
+    words = read_word_file(infile)
+    words.sort()
+
+    out = infile
+    if outfile is not None:
+        out = outfile
+
+    with open(out, 'w') as writefile:
+        for word in words:
+            writefile.write(word)
+            writefile.write("\n")
+
+# Basic Test Data
 sentence = 'Throw Dirty rock at that mangey old goblin'
 print("testing input stentence:", sentence)
 
-ip = parser.InputParser()
+# Init the Game Dictionary
+nouns = read_word_file("data/nouns.txt")
+verbs = read_word_file("data/verbs.txt")
+prepositions = read_word_file("data/prepositions.txt")
+adjectives = read_word_file("data/adjectives.txt")
+game_dictionary = taps.GameDictionary(adjectives, [], nouns, prepositions, verbs)
+
+# Create Parser
+ip = taps.InputParser(game_dictionary)
+
+# Parse Sentence.
 ip.parse_input(sentence)
